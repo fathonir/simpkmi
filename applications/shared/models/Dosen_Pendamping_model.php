61d416ed453ae7ce38327c5e4512ab5506ba4915
Fathoni <m.fathoni@mail.com>
2020-09-04 10:09:17 +0700

    fungsi get_from_dosen

--- applications/shared/models/Dosen_Pendamping_model.php
+++ applications/shared/models/Dosen_Pendamping_model.php
@@ -8,7 +8,7 @@ class Dosen_Pendamping_model extends CI_Model
 {
 	public function get_from_dosen($dosen_id, $kegiatan_id)
 	{
-		return $this->db->get_where([
+		return $this->db->get_where('dosen_pendamping', [
 			'dosen_id' => $dosen_id,
 			'kegiatan_id' => $kegiatan_id
 		])->first_row();
