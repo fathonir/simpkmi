9a829fc67624ea473bd6cff58c2ab44615523537
Fathoni <m.fathoni@mail.com>
2021-03-07 22:41:23 +0700

    menambahkan fitur download excel peserta online workshop

--- applications/admin/controllers/Online_workshop.php
+++ applications/admin/controllers/Online_workshop.php
@@ -1,5 +1,8 @@
 <?php
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 /**
  * @author Fathoni <m.fathoni@mail.com>
  * @property Kegiatan_model $kegiatan_model 
@@ -45,5 +48,36 @@ class Online_workshop extends Admin_Controller
 		}
 		
 		echo json_encode($meeting_set);
+	}
+
+	public function excel_pendaftar()
+	{
+		$meeting = $this->meeting_model->get_single($this->input->get('meeting_id'));
+		$data_set = $this->peserta_model->list_all_by_meeting($this->input->get('meeting_id'));
+
+		$spreadsheet = new Spreadsheet();
+		$sheet = $spreadsheet->getActiveSheet();
+		$sheet->setCellValue('A1', 'Perguruan Tinggi');
+		$sheet->setCellValue('B1', 'NIM');
+		$sheet->setCellValue('C1', 'Nama');
+		$sheet->setCellValue('D1', 'Status Kehadiran');
+		$sheet->setCellValue('E1', 'Username');
+		$sheet->setCellValue('F1', 'Password');
+
+		for ($i = 0; $i < count($data_set); $i++)
+		{
+			$row = $i + 2;
+			$sheet->setCellValue('A'.$row, $data_set[$i]->nama_pt);
+			$sheet->setCellValue('B'.$row, $data_set[$i]->nim);
+			$sheet->setCellValue('C'.$row, $data_set[$i]->nama);
+			$sheet->setCellValue('D'.$row, $data_set[$i]->kehadiran);
+			$sheet->setCellValue('E'.$row, $data_set[$i]->username);
+			$sheet->setCellValue('F'.$row, $data_set[$i]->password);
+		}
+
+		header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		header("Content-Disposition: attachment; filename=\"Peserta Online Workshop {$meeting->waktu_mulai}.xlsx\"");
+		$writer = new Xlsx($spreadsheet);
+		$writer->save('php://output');
 	}
 }
